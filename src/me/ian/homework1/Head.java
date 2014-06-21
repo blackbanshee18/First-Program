c63fc9908937a1f45af93d2d1fa48cf3a9675334
blackbanshee18 <ian.macaig@gmail.com>
2014-06-21 17:27:27 -0400

    First Commit

---
+++ src/me/ian/homework1/Head.java
@@ -0,0 +1,12 @@
+package me.ian.homework1;
+
+public class Head 
+{
+	int Billy=65;
+			
+	int Joey=43;
+	
+	int BillyAte=5;
+	
+	int JoeyAte=10;
+}
---
+++ src/me/ian/homework1/Oranges.java
@@ -0,0 +1,12 @@
+package me.ian.homework1;
+
+public class Oranges {
+
+	public static void EatOranges (String[] args)
+	{
+		int ToEat=
+		
+		int total=
+		
+		System.out.println("The number of Oranges Eaten:"+ToEat-total);
+}
